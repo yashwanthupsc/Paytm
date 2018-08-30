0bf6c75dd1219b9e1999574711a90c0cb04506f2
Yashwanth Manchala <yashwanthupsc@gmail.com>
2018-08-30 12:49:00 +0530

    Creating containers using Docker.sh file from jenkins

---
+++ Doccont.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cont_count=$1
+echo "creating $cont_count containers"
+sleep 2;
+for i in `seq $cont_count`
+do
+	echo "================================"
+	echo "creating www.Paytm$i conatiner.."
+        sleep 1
+        docker run --name www.paytm$i -d -it -rm yashwanthupsc/ssh /bin/bash
+        echo "www.paytm$i container has been created"
+        echo "======================================="
+done
